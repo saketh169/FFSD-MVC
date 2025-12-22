8df3c941f027f16d26dbeb11f99eb53adab3e00f
saketh169 <sakethrupa123@gmail.com>
2025-12-22 13:09:13 +0530

    update

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost:27017/NutriConnectDB" ;
+    const MONGODB_URI = process.env.MONGODB_URL;
     if (!MONGODB_URI) {
       throw new Error('MONGO_URI is not defined');
     }
